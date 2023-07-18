68523eda609b975430fca3869fcac0bbabcbb341
özcan <ozxcan91@gmail.com>
2023-07-18 14:00:44 +0200

    2. degisim

--- src/C00_1.java
+++ src/C00_1.java
@@ -2,5 +2,6 @@ public class C00_1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello GitHub!");
+        System.out.println("Merhaba Dünya!");
     }
 }
